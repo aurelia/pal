f3c9c995003eff0eafd0d3885426d620b77d0086
Rob Eisenberg <rob@bluespire.com>
2015-09-27 13:27:45 -0400

    feat(all): add interface definitions

--- src/index.js
+++ src/index.js
@@ -20,11 +20,62 @@ export function AggregateError(message: string, innerError?: Error, skipIfAlread
   return e;
 }
 
-export FEATURE = {};
-export DOM = {};
-export PLATFORM = {};
+interface Feature {
+  shadowDOM: boolean;
+  htmlTemplateElement: boolean;
+  objectObserve: boolean;
+  arrayObserve: boolean;
+}
 
-export function initializePAL(platform, feature, dom) {
+export const FEATURE: Feature = {};
+
+interface Platform {
+  location: Object;
+  history: Object;
+  XMLHttpRequest: XMLHttpRequest;
+  findModuleForExport(exp: any): Object;
+}
+
+export const PLATFORM: Platform = {};
+
+PLATFORM.global = (function() {
+  // Workers don’t have `window`, only `self`
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+
+  // Not all environments allow eval and Function
+  // Use only as a last resort:
+  return new Function('return this')();
+})();
+
+interface Dom {
+  Element: Element;
+  boundary: string;
+  adoptNode(node: Node): Node;
+  createElement(tagName: string): Element;
+  createTextNode(text: string): Text;
+  createComment(text: string): Comment;
+  createDocumentFragment(): DocumentFragment;
+  createMutationObserver(callback: Function): MutationObserver;
+  createCustomEvent(eventType: string, options: Object): CustomEvent;
+  getComputedStyle(element: Element): ComputedStyle;
+  getElementById(id: string): Element;
+  querySelectorAll(query: string): NodeList;
+  nextElementSibling(element: Node): Element;
+  createTemplateFromMarkup(markup: string): Element;
+  replaceNode(newNode: Node, node: Node, parentNode: Node): void;
+  removeNode(node: Node, parentNode: Node): void;
+  injectStyles(styles: string, destination?: Element, prepend?:boolean): Node;
+}
+
+export const DOM: Dom = {};
+
+export function initializePAL(platform: Platform, feature: Feature, dom: Dom): void {
   Object.assign(PLATFORM, platform);
   Object.assign(FEATURE, feature);
   Object.assign(DOM, dom);
