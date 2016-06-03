adc7e905abf4f76d1c71a19f740cb6bb45d5842e
Rob Eisenberg <rob@bluespire.com>
2016-06-03 16:11:41 -0400

    chore(all): update built and lint

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -5,8 +5,18 @@ var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 var rename = require('gulp-rename');
+var tools = require('aurelia-tools');
+var through2 = require('through2');
 
 var jsName = paths.packageName + '.js';
+
+function cleanGeneratedCode() {
+  return through2.obj(function(file, enc, callback) {
+    file.contents = new Buffer(tools.cleanGeneratedCode(file.contents.toString("utf8")));
+    this.push(file);
+    return callback();
+  })
+}
 
 gulp.task('build-index', function(){
   return gulp.src(paths.root + 'index.js')
@@ -17,24 +27,28 @@ gulp.task('build-index', function(){
 gulp.task('build-es2015', function () {
   return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions.es2015())))
+    .pipe(cleanGeneratedCode())
     .pipe(gulp.dest(paths.output + 'es2015'));
 });
 
 gulp.task('build-commonjs', function () {
   return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions.commonjs())))
+    .pipe(cleanGeneratedCode())
     .pipe(gulp.dest(paths.output + 'commonjs'));
 });
 
 gulp.task('build-amd', function () {
   return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions.amd())))
+    .pipe(cleanGeneratedCode())
     .pipe(gulp.dest(paths.output + 'amd'));
 });
 
 gulp.task('build-system', function () {
   return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions.system())))
+    .pipe(cleanGeneratedCode())
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
