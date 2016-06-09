4991d85fc15299e390feb86069a108df70cd85ca
Rob Eisenberg <rob@bluespire.com>
2016-06-10 00:30:16 +0200

    chore(doc): update api doc build

--- build/tasks/doc.js
+++ build/tasks/doc.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var typedoc = require('gulp-typedoc');
-var typedocExtractor = require('gulp-typedoc-extractor');
 var runSequence = require('run-sequence');
+var through2 = require('through2');
 
 gulp.task('doc-generate', function(){
   return gulp.src([paths.output + paths.packageName + '.d.ts'])
@@ -18,16 +18,28 @@ gulp.task('doc-generate', function(){
     }));
 });
 
-gulp.task('doc-extract', function(){
+gulp.task('doc-shape', function(){
   return gulp.src([paths.doc + '/api.json'])
-    .pipe(typedocExtractor(paths.packageName))
+    .pipe(through2.obj(function(file, enc, callback) {
+      var json = JSON.parse(file.contents.toString('utf8')).children[0];
+
+      json = {
+        name: paths.packageName,
+        children: json.children,
+        groups: json.groups
+      };
+
+      file.contents = new Buffer(JSON.stringify(json));
+      this.push(file);
+      return callback();
+    }))
     .pipe(gulp.dest(paths.doc));
 });
 
 gulp.task('doc', function(callback){
   return runSequence(
     'doc-generate',
-    'doc-extract',
+    'doc-shape',
     callback
   );
 });
